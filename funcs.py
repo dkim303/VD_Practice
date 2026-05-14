2e385338b872514a462d37408c1e7fbbb9983d45
Darius Kim <dariuskim@Dariuss-MacBook-Pro.local>
2026-05-13 21:05:13 -0700

    added new files

---
+++ funcs.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+def add_vectorized_page(url, array):
+	url = "https://en.wikipedia.org/wiki/July_Crisis"
+
+	response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+	response.raise_for_status()
+
+	soup = BeautifulSoup(response.text, "html.parser")
+
+	paragarphs = soup.find_all("p")
+
+	for p in paragarphs:
+    	text = p.get_text(strip=True)
+
+	
