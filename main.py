261db9c9a81b899b610dde07cf386e73f8e85e26
Darius Kim <dariuskim@mac.mynetworksettings.com>
2026-05-15 15:50:09 -0700

    major work to user-interface

--- main.py
+++ main.py
@@ -5,81 +5,48 @@ from vector_db_package import utils
 import requests
 import json
 import sys
+from pathlib import Path
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 if __name__ == "__main__":
     print("Load in embeddings matrix and metadata")
     e_name = input("Enter embeddings file name: ")
+    d_name = input("Enter metadata file name: ")
+
     try:
-        embeddings_matrix = np.load(e_name)
+        embeddings_path = Path(e_name)
+        data_path = Path(d_name)
+
+        # check for mismatch in file existance
+        if (embeddings_path.exists() == False and data_path.exists() == True):
+            raise Exception("Error: Embeddings file does not exist but Data file exists.")
+        elif (embeddings_path.exists() == True and data_path.exists() == False):
+            raise Exception("Error: Data file does not exist but Embeddings file exists.")   
+        
+        # later on add function to check if all items in the 2 files match correctly
+
+        if embeddings_path.exists():
+            embeddings_matrix = np.load(e_name)
+        # case of creating empty matrix, dim-384, num vectors is 0
+        else:
+            embeddings_matrix = np.empty((0,384), dtype=np.float32)
+
+        if data_path.exists():
+            with open(d_name) as data_fp:
+                data = json.load(data_fp)
+                data = {int(k): v for k, v in data.items()}
+        else:
+        # case of creating empty data list, no data recorded yet in JSON format
+            data = {}
+
     except Exception as e:
-        print("Error: Failed to read embeddings file.")
+        print(e)
         sys.exit(1)
 
-    d_name = input("Enter metadata file name: ")
-    try:
-        with open(d_name) as meta_fp:
-            metadata = json.load(meta_fp)
-            metadata = {int(k): v for k, v in metadata.items()}
-
-    except Exception as e:
-            print("Error: Failed to read metadata file")
-            sys.exit(1)
-
-    utils.menu()
-    selected_num = input()
-
-    try:
-        match selected_num:
-
-            # Making A Query
-            case 1:
-                query = input("Enter query: ")
-                query_vector = model.encode(query, convert_to_numpy=True, normalize_embeddings=True)
-
-            case 2:
-                should_continue = True
-                while should_continue:
-                    mode = input("Enter 0 to log a Website or 1 to log a PDF.")
-
-                    # Website URL Entering
-                    if mode == 0:
-                        url = input("Enter new book URL: ")
-                        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
-                        if response.raise_for_status == False:
-                            raise ValueError("Error: invalid website URL given.")
-                        
-                        is_duplicate = utils.check_duplicate(url, metadata)
-                        if is_duplicate == True:
-                            raise ValueError("Error: URL already logged.")
-                        
-                    # PDF URL Entering
-                    elif mode == 1:
-                        url = input("Enter new website URL: ")
-                        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
-                        if response.raise_for_status == False:
-                            raise ValueError("Error: invalid pdf URL given.")
-                        
-                        is_duplicate = utils.check_duplicate(url, metadata)
-                        if is_duplicate == True:
-                            raise ValueError("Error: URL already logged.")
-
-                    # Invalid URL Type
-                    else:
-                        raise ValueError("Error: Invalid URL type entered.")
-                    
-                    entered_message = input("Enter DONE if done entering in data.")
-                    if entered_message == "DONE":
-                        should_continue = False
-                    else:
-                        should_continue = True
-
-            # Invalid Option Entered
-            case _:
-                raise ValueError("Error: invalid command given.") 
-            
-    except ValueError as e:
-        print(e)
+    should_continue = True
+    while should_continue:
+        # handles all user inputs for command sequences and program quitting
+        should_continue = utils.interface(embeddings_matrix, data, e_name, d_name)
             
     
--- vector_db_package/utils.py
+++ vector_db_package/utils.py
@@ -4,11 +4,17 @@ import requests
 from sentence_transformers import SentenceTransformer
 from pypdf import PdfReader
 from io import BytesIO
+import requests
+import json
+import sys
+from pathlib import Path
+
 
 def menu():
 	print(f"Enter 1 to make query.")
 	print("Enter 2 to add new URL to database.")
 	print("Enter 3 to add new PDF to database.")
+	print("Enter q to quit.")
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
@@ -30,14 +36,14 @@ def add_vectorized_website(url, vectors, dictionary):
 		dictionary[len(dictionary)] = { "url": url, "text": text}
 
 def chunk_text(text, max_words=500):
-    words = text.split()
-    chunks = []
+	words = text.split()
+	chunks = []
 
-    for i in range(0, len(words), max_words):
-        chunk = " ".join(words[i:i + max_words])
-        chunks.append(chunk)
+	for i in range(0, len(words), max_words):
+		chunk = " ".join(words[i:i + max_words])
+		chunks.append(chunk)
 
-    return chunks
+	return chunks
 
 def add_vectorized_PDF(url, vectors, dictionary):
 	response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
@@ -78,3 +84,73 @@ def search_database(query_vector, vectors, dictionary, K):
 	for vector in vectors:
 		similarity_coefficient = cosine_compare(query_vector, vector)
 		
+
+def interface(embeddings, metadata, e_name, d_name):
+	
+	# give user options on terminal
+	menu()
+	selected_num = input()
+
+	try:
+		match str(selected_num):
+
+			# Making A Query
+			case "1":
+				query = input("Enter query: ")
+				query_vector = model.encode(query, convert_to_numpy=True, normalize_embeddings=True)
+
+				# make query here
+
+				return True
+			
+			case "2":
+				should_continue = True
+				while should_continue:
+					mode = input("Enter 0 to log a Website or 1 to log a PDF.")
+
+					# Website URL Entering
+					if mode == 0:
+						url = input("Enter new book URL: ")
+						response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
+						if response.raise_for_status == False:
+							raise ValueError("Error: invalid website URL given.")
+						
+						is_duplicate = check_duplicate(url, metadata)
+						if is_duplicate == True:
+							raise ValueError("Error: URL already logged.")
+						
+						# log URL here
+						return True
+						
+					# PDF URL Entering
+					elif mode == 1:
+						url = input("Enter new website URL: ")
+						response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
+						if response.raise_for_status == False:
+							raise ValueError("Error: invalid pdf URL given.")
+						
+						is_duplicate = check_duplicate(url, metadata)
+						if is_duplicate == True:
+							raise ValueError("Error: URL already logged.")
+						
+						# log URL here
+						return True
+
+					# Invalid URL Type
+					else:
+						print("Error: Invalid URL type entered.")
+						return True
+
+			# Exit program
+			case "q":
+				print("Quitting Program.")
+				return False
+
+			# Invalid Option Entered
+			case _:
+				raise ValueError("Error: invalid command given.")
+				return False
+
+	except ValueError as e:
+		print(e)
+		return False
